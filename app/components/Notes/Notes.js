001d37ad351e6ca9dcf0de4d4f213fc553af2a88
Teriq Perry <jtmp2r@gmail.com>
2016-05-16 14:13:42 -0500

    notes component updated

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,12 +1,13 @@
 var React = require("react");
+var NotesList = require('./NotesList')
 
 
 var Notes = React.createClass({
 	render: function() {
-		console.log('Notes:', this.props.notes);
 		return (
 			<div>
-				<p>Que?</p>
+				<h3>Notes for {this.props.username}</h3>
+				<NotesList notes={this.props.notes} />
 			</div>
 		)
 	}
